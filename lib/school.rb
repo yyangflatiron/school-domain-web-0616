66a03fbd161d6931894b21c06a5bb49d0fb0bac1
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-14 12:47:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.keys.include?(grade) == false
+      @roster[grade]=[]
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, students|
+      roster[grade]=students.sort
+    end
+    roster
+  end
+
+end
+#school = School.new("Bayside High School")
